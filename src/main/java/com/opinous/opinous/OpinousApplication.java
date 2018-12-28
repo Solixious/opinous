879b8e8a6f758618a324f7994cd31437bdb9ec9f
Pratyush <pratyush@Pratyushs-MacBook-Pro.local>
2018-12-28 10:06:34 +0530

    First commit

---
+++ src/main/java/com/opinous/opinous/OpinousApplication.java
@@ -0,0 +1,14 @@
+package com.opinous.opinous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpinousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpinousApplication.class, args);
+	}
+
+}
+
