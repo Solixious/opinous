2954bcc4e9d71d08f024011abbe5c29c51085ae5
Pratyush <pratyush@Pratyushs-MacBook-Pro.local>
2018-12-28 21:57:45 +0530

    crap commit

--- src/main/java/com/opinous/controller/UserController.java
+++ src/main/java/com/opinous/controller/UserController.java
@@ -43,7 +43,7 @@ public class UserController {
 
         securityService.autologin(userForm.getUsername(), userForm.getPassword());
 
-        return "redirect:/welcome";
+        return "/welcome";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
