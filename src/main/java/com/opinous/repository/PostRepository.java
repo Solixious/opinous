4c33dc3503ac2580077e77a6a87dabd634837a6c
Pratyush Choudhary <solixious@gmail.com>
2019-02-02 23:14:49 +0530

    Fixed the order in which the posts in a room appears

--- src/main/java/com/opinous/repository/PostRepository.java
+++ src/main/java/com/opinous/repository/PostRepository.java
@@ -3,10 +3,12 @@ package com.opinous.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.opinous.model.AnonMap;
 import com.opinous.model.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long>  {
+	@Query("select p from Post p where anonMap in :anonMaps order by createDate desc")
 	public List<Post> findByAnonMapIn(List<AnonMap> anonMaps);
 }
