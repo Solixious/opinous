9b1ae7956f6ad6df18e80f5f01b7955bd9c8e6f6
Pratyush Choudhary <solixious@gmail.com>
2019-03-11 15:28:30 +0530

    Added logs to AppConfigServiceImpl

--- src/main/java/com/opinous/service/impl/AppConfigServiceImpl.java
+++ src/main/java/com/opinous/service/impl/AppConfigServiceImpl.java
@@ -1,5 +1,6 @@
 package com.opinous.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +8,7 @@ import com.opinous.model.AppConfiguration;
 import com.opinous.repository.AppConfigRepository;
 import com.opinous.service.AppConfigService;
 
+@Slf4j
 @Service
 public class AppConfigServiceImpl implements AppConfigService {
 
@@ -15,15 +17,24 @@ public class AppConfigServiceImpl implements AppConfigService {
 	
 	@Override
 	public String getAppConfig(String key, String defaultValue) {
+		if(key == null) {
+			log.error("Cannot retrieve app configuration for a null key value.");
+			return null;
+		}
+
 		AppConfiguration appConfig = appConfigRepository.findByKey(key);
 		if(appConfig == null) {
+
 			appConfig = new AppConfiguration();
 			appConfig.setKey(key);
 			appConfig.setValue(defaultValue);
+
 			appConfigRepository.save(appConfig);
+			log.info("Added new app config with key: {}, value: {}", key, defaultValue);
+
 			return defaultValue;
 		}
+
 		return appConfig.getValue();
 	}
-
 }
